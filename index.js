7cc67504911b3902d6278bc78fa8ff8c5da62f7e
Walter Scarano <walterscarano@outlook.com>
2018-12-16 12:18:25 -0300

    Support for multiple pools

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const pool = require('async-functions-pool')();
+const createPool = require('async-functions-pool');
 const amphtmlValidator = require('amphtml-validator');
 const fetch = require('node-fetch');
 
-module.exports = async (urlGenerator, parallel = 1) => {
-  const validator = await amphtmlValidator.getInstance();
-  for(url of urlGenerator()){
-    pool.add(makeJob(validator, url));
+module.exports = () => {
+    const pool = createPool();
+    return async (urlGenerator, parallel = 1) => {
+    const validator = await amphtmlValidator.getInstance();
+    for(url of urlGenerator()){
+      pool.add(makeJob(validator, url));
+    }
+    return pool.run(parallel);
   }
-  return pool.run(parallel);
 }
 
 const makeJob = (validator, url) =>
